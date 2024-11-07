feae77cb75e2c38714d2091c73a5acf11c89aa10
ArieGunawanPriadji <ariegunawanpriadji@gmail.com>
2024-11-07 13:53:54 +0700

    Project.py

--- Project.py
+++ Project.py
@@ -1,1 +1,5 @@
-#code python
+kendaraan=["*" for i in range (4)]
+for i in range (4):
+  kendaraan[i]=str(input()) #saat tap kartu data kendaraan tercatat [plat nomor, jenis kendaraan, gerbang masuk, saldo e-toll]
+print ("sisa saldo e-toll anda sebesar Rp.", kendaraan[4] ) # gerbang terbuka
+
